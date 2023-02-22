69249f7420e7d9615a8b033a96bb14327ac80629
luzidao <126052798+luzidao@users.noreply.github.com>
2023-02-22 15:05:52 +0800

    Create test.3.cmd

---
+++ test.3.cmd
@@ -0,0 +1,152 @@
+```python
+a_list = [2,3,7,None]
+```
+
+
+```python
+tup = ['foo','bar,','hhh']
+```
+
+
+```python
+b_list = list(tup)
+```
+
+
+```python
+b_list
+```
+
+
+
+
+    ['foo', 'bar,', 'hhh']
+
+
+
+
+```python
+b_list[1]= 'peek'
+```
+
+
+```python
+b_list
+```
+
+
+
+
+    ['foo', 'peek', 'hhh']
+
+
+
+
+```python
+gen = range(10)
+gen
+```
+
+
+
+
+    range(0, 10)
+
+
+
+
+```python
+list(gen)
+```
+
+
+
+
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+
+```python
+b_list.append('fafsa')
+b_list
+```
+
+
+
+
+    ['foo', 'peek', 'hhh', 'fafsa', 'fafsa']
+
+
+
+
+```python
+b_list.insert(2,'faklfjkas')
+b_list
+```
+
+
+
+
+    ['foo', 'peek', 'faklfjkas', 'hhh', 'fafsa', 'fafsa']
+
+
+
+
+```python
+'peek' in b_list
+```
+
+
+
+
+    True
+
+
+
+
+```python
+'peel' in b_list
+```
+
+
+
+
+    False
+
+
+
+
+```python
+
+```
+
+
+```python
+
+```
+
+
+```python
+
+```
+
+
+```python
+
+```
+
+
+```python
+
+```
+
+
+```python
+
+```
+
+
+```python
+
+```
